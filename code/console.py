68241b81b6c251d8422dcd0e514b7b0137a8642b
adamalbu <albu.adam@gmail.com>
2021-02-14 12:22:58 +0000

    code

--- code/console.py
+++ code/console.py
@@ -1,2 +1,11 @@
 print('Hi.')
-print('Welcome to everypy where you can do lots of things')
+print('Welcome to everypy where you can do lots of things.')
+print('Type help when you need some help.')
+cmd = ""
+while cmd != "shutdown":
+    cmd = input("P:/>")
+    splitcmd = cmd.split()
+    if splitcmd[0] == "help":
+        print('help')
+        print('shutdown')
+    
